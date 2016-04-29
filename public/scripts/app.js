1b3af1ad1f80a6e5fc7e000a0da057498c86c740
Jacques Uys <jaxuys@gmail.com>
2016-04-29 17:28:23 +0200

    http service consuming the data

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,13 @@
 angular.module('DOS', [])
+.controller('allUsersCtrl', function($http, $scope) {
+	$http({
+    method: 'GET',
+    url: '/data/data.json'
+  })
+  .then(function (resp) {
+    console.log(resp.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
+});

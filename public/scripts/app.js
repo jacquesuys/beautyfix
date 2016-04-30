f5360e77720ed4c8452abe7b21961854dac125f2
Jacques Uys <jaxuys@gmail.com>
2016-04-30 10:55:25 +0200

    routing for app

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,14 @@
-angular.module('DOS', [])
+angular.module('DOS', ['ngRoute'])
+.config(function ($routeProvider, $httpProvider) {
+  $routeProvider
+    .when('/users', {
+      templateUrl: 'template/users.html',
+      controller: 'allUsersCtrl'
+    })
+    .otherwise({
+      redirectTo: '/users'
+    });
+})
 .controller('allUsersCtrl', function($http, $scope) {
 	$http({
     method: 'GET',

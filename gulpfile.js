2e61f5f1c07b3c3b2c85276ea3c839de5cfb3fe1
Jacques Uys <jaxuys@gmail.com>
2016-04-30 12:43:03 +0200

    sass tasks included, new default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 var gulp = require("gulp");
 var connect = require("gulp-connect");
+var sass = require("gulp-sass");
+var concat = require("gulp-concat");
+
+gulp.task("sass", function () {
+  return gulp.src('./public/sass/style.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./public/styles/'));
+});
+
+gulp.task("sass:watch", function () {
+  gulp.watch('./public/sass/**/*.scss', ['sass']);
+});
 
 gulp.task("start", function () {
-
   connect.server({
     root: "public",
     port: 3000,
@@ -14,3 +26,5 @@ gulp.task("start", function () {
     }
   });
 });
+
+gulp.task('default', ['sass', 'start']);

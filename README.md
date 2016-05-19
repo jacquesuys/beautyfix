74948c72f8242e84bb149f7bbf1e91e31e503e87
Jacques Uys <jaxuys@gmail.com>
2016-05-19 05:15:29 +0200

    base camp for beautyfix

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# DOS
+# Beautyfix
 
-A simple Angular application, that lists and displays user(s) information, allows for filtering and sorting.
-
-Run `npm install -g bower grunt-cli` `npm install` `bower install`
+Please fill this out!
--- gulpfile.js
+++ gulpfile.js
@@ -16,12 +16,7 @@ gulp.task("start", function () {
   connect.server({
     root: "public",
     port: 3000,
-    livereload: true,
-    middleware: function(connect, opt) {
-      return [
-        connect().use('/bower_components', connect.static('./bower_components'))
-      ]
-    }
+    livereload: true
   });
 });
 

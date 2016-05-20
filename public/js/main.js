dfb39c88f509dee354f52fe00f2dc94a1489439b
Jacques Uys <jaxuys@gmail.com>
2016-05-20 13:27:32 +0200

    basic menu styling using bebas

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,12 @@ $(document).ready(function(){
     console.log(data);
   })
   .fail(function(err){
-    console.log(err);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position){
+        // check if it is outside the geo bounds, assign to the UK
+        console.log(position.coords.latitude + ' ' + position.coords.longitude);
+      });
+    }  
   });
 
   $('header > span').on('click', function(){

db9b1fcf5855531232d4fb070e12605ffc7643d2
Jacques Uys <jaxuys@gmail.com>
2016-05-23 13:45:32 +0200

    show stockist according to location

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,23 @@
 $(document).ready(function(){
-  // fallback: http://ip-api.com/json
-  $.get('http://freegeoip.net/json/')
-  .done(function(data){
-    console.log(data);
-    var country = data["country_name"];
-    console.log(country);
-  })
-  .fail(function(err){
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position){
-        // check if it is outside the geo bounds, assign to the UK
-        console.log(position.coords.latitude + ' ' + position.coords.longitude);
-      });
-    }  
-  });
+  var $location = sessionStorage.getItem('country');
+
+  if (!$location) {
+    // fallback: http://ip-api.com/json
+    $.get('http://freegeoip.net/json/')
+    .done(function(data){
+      console.log(data);
+      var country = data["country_name"];
+      sessionStorage.setItem('country', country);
+    })
+    .fail(function(err){
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position){
+          // check if it is outside the geo bounds, assign to the UK
+          console.log(position.coords.latitude + ' ' + position.coords.longitude);
+        });
+      }  
+    });
+  }
 
   $('header > span').on('click', function(){
     $('body').toggleClass('nav-open');
@@ -24,5 +28,14 @@ $(document).ready(function(){
       $('body').removeClass('nav-open');
     }
   });
+
+  // if not ZA show UK
+  if ($location !== "South Africa") {
+    $('.za').hide();
+    $('.uk').show();
+  } else {
+    $('.za').show();
+    $('.uk').hide();
+  }
 
 });

7a96223e85b525e01ddc359a9e35ab8cc5c8afed
Jacques Uys <jaxuys@gmail.com>
2016-06-20 18:09:17 +0200

    IE fixes

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,36 @@
 $(document).ready(function(){
-  var $location = sessionStorage.getItem('country');
-
-  if (!$location) {
-    // fallback: http://ip-api.com/json
-    $.get('http://freegeoip.net/json/')
-    .done(function(data){
-      console.log(data);
-      var country = data["country_name"];
-      sessionStorage.setItem('country', country);
-    })
-    .fail(function(err){
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position){
-          // check if it is outside the geo bounds, assign to the UK
-          console.log(position.coords.latitude + ' ' + position.coords.longitude);
-        });
-      }  
-    });
+  var $location;
+  if (!!sessionStorage) {
+    $location = sessionStorage.getItem('country');
+  } else {
+    $location = undefined;
   }
+
+  var getGeo = function() {
+    if (!$location) {
+      // fallback: http://ip-api.com/json
+      $.get('http://freegeoip.net/json/')
+      .done(function(data){
+        console.log(data);
+        var country = data["country_name"];
+        if (!!sessionStorage) {
+          sessionStorage.setItem('country', country);
+        } else {
+          $location = country;
+        }
+      })
+      .fail(function(err){
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position){
+            // check if it is outside the geo bounds, assign to the UK
+            console.log(position.coords.latitude + ' ' + position.coords.longitude);
+          });
+        }  
+      });
+    }
+  }
+
+  getGeo();
 
   $('header > span').on('click', function(){
     $('body').toggleClass('nav-open');
@@ -31,7 +44,12 @@ $(document).ready(function(){
 
   var showStockists = function() {
     // if not ZA show UK
-    var $location = sessionStorage.getItem('country');
+    if (!!sessionStorage) {
+      $location = sessionStorage.getItem('country');
+    } else {
+      getGeo();
+    }
+
     if ($location !== "South Africa") {
       $('.za').hide();
       $('.uk').show();
@@ -48,7 +66,11 @@ $(document).ready(function(){
   $('.region > span a').on('click', function(e){
     e.preventDefault();
     var country = $(this).data('country');
-    sessionStorage.setItem('country', country);
+    if (!!sessionStorage) {
+      sessionStorage.setItem('country', country);
+    } else {
+      $location = country;
+    }
     showStockists();
   });
 
